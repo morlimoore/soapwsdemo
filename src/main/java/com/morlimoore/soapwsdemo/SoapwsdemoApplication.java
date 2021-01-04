74bf90ec06c714b4802747feca528e6324d2465c
morlimoore <cooliyk2@gmail.com>
2021-01-04 05:57:06 +0100

    Initial commit

---
+++ src/main/java/com/morlimoore/soapwsdemo/SoapwsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.morlimoore.soapwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapwsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapwsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morlimoore/soapwsdemo/SoapwsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morlimoore.soapwsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapwsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
